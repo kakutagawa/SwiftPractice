750f6080ebad2624d58dca0021482490dd006151
yossibank <yossibank@yahoo.co.jp>
2022-08-13 16:29:41 +0900

    インデント調整

--- SwiftPractice/ViewController.swift
+++ SwiftPractice/ViewController.swift
@@ -1,8 +1,7 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 }
