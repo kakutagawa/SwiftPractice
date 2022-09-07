bb2516c01925bf19f32894c6711b575db843cc82
kakutagawa <kaktgw0824@gmail.com>
2022-09-07 22:37:05 +0900

    chapter0課題完了

--- SwiftPractice/Chapter/Chapter0/ChapterZeroViewController.swift
+++ SwiftPractice/Chapter/Chapter0/ChapterZeroViewController.swift
@@ -3,5 +3,14 @@ import UIKit
 final class ChapterZeroViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        button.setTitle("ボタンチェンジ", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        
     }
+    @IBOutlet weak var label: UILabel!
+    @IBAction func tappedButton(_ sender: Any) {
+        label.text = "Goodbye World!"
+    }
+    @IBOutlet weak var button: UIButton!
 }
