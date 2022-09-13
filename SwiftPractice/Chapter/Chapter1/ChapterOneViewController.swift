ddfc722e7d3b8a1c68dcb35c58e54ef2bf1b2bee
kakutagawa <kaktgw0824@gmail.com>
2022-09-13 23:46:07 +0900

    Chapter1課題完了

--- SwiftPractice/Chapter/Chapter1/ChapterOneViewController.swift
+++ SwiftPractice/Chapter/Chapter1/ChapterOneViewController.swift
@@ -1,7 +1,18 @@
 import UIKit
 
-final class ChapterOneViewController: UIViewController {
+final class ChapterOneViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textField.delegate = self
+        
+        textField.placeholder = "テキスト"
+        textField.textColor = .red
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("returnが押されました")
+        return true
     }
 }
